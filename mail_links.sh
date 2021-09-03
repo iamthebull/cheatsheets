8942c970379b972e0d284296de71330827f5451e
David Bull <david.bull@apacsystems.ca>
2021-09-02 19:16:20 -0600

    Added thunderbird insall notes

---
+++ mail_links.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# create symbolic links to mail folders
+
+SOURCEDIR=/data/Mail/
+
+for TYPE in Inbox Sent
+do
+  for YEAR in {2008..2019}
+  do
+  SOURCE=$SOURCEDIR"$TYPE$YEAR"
+  DEST=./"$TYPE$YEAR"
+  echo $SOURCE
+  echo $DEST
+  ln -s $SOURCE $DEST
+  done
+done
+exit 0
