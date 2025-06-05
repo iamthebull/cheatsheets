45b43aaa37bad8d6c2421b5ce5c1959aca3ae78f
David Bull <david.bull@apacsystems.ca>
2025-06-05 10:02:43 +0300

    updated freecad

--- freecad-launch.sh
+++ freecad-launch.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-version=1.0.1
-homedir=/home/david/.config/FreeCAD
-usr=$homedir/user_$version.cfg
-sys=$homedir/system_$version.cfg
-fcdir=/media/david/MyDocs/freecad
-run=$fcdir/appimage/freecad-$version/AppRun
-home=$fcdir/projects
-cd $home
-$run -u $usr -s $sys
+VERSION=1.0.1
+FCDIR=/media/david/MyDocs/freecad/appimage/freecad-$VERSION
+export FREECAD_USER_HOME="$FCDIR-home"
+cd /home/david/freecad
+$FCDIR/AppRun
